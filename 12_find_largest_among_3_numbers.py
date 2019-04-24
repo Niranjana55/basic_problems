bf423bf6be3da37d77fac54a23b09ccf0fd34297
Niranjana55 <46274625+Niranjana55@users.noreply.github.com>
2019-04-24 15:21:31 +0530

    basic prg

---
+++ 12_find_largest_among_3_numbers.py
@@ -0,0 +1,10 @@
+#find largest among 3 numbers
+a=int(input("enter the number a:"))
+b=int(input("enter the number b:"))
+c=int(input("enter the number c:"))
+if(a>b):
+    print("a is largest number")
+elif(b>c):
+    print("b is largest number")
+else:
+    print("c is largest number")
